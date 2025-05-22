b2b07a65b0a9bf40f1178c1d612a8b6e97a2a231
Juan Arias <juandavidarias378@gmail.com>
2025-05-22 11:56:42 -0500

    Tablas de multiplicacion

---
+++ PYx02/8-tablas_multiplicar.py
@@ -0,0 +1,17 @@
+# Debes repasar las tablas de multiplicar del 1 al 5.
+# Haz un programa que te permita ver cada tabla de multiplicar una por una.
+
+opcion = ''
+
+for tabla_multiplicacion in range(1, 6): # Maldito seas y seras
+    while opcion != 'no':
+        opcion = input('¿Deseas ver la siguiente tabla?: ').lower()
+        if opcion == 'si':
+            for numero in range(1, 11):
+                tablas =  tabla_multiplicacion * numero
+                print(f'{tabla_multiplicacion}*{numero}={tablas}')
+            break
+        elif opcion == 'no':
+            print('Programa finalizado')
+        else:
+            print('la opcion ingresada no es valida, Vuelve a intentarlo')
