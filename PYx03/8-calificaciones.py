fe8cecc72949c57d44f70ca8364acd2715119f37
Juan Arias <juandavidarias378@gmail.com>
2025-05-28 12:19:49 -0500

    Finalizacion de la funcion de analizar calificaciones

--- PYx03/8-calificaciones.py
+++ PYx03/8-calificaciones.py
@@ -3,34 +3,32 @@
 # (ej: {"Ana": 4.5, "Luis": 3.7} ) y devuelva:
 calificaciones = {
     'Ana': [2.5, 3.0, 4.8, 2.1, 5.0],
-    'Luis': [5.0, 1.0, 4.0, 3.3, 4.2, 3.1]
+    'Luis': [2.0, 5.0, 2.0, 3.3, 4.2, 3.0],
+    'Carlos': [2.0, 1.0, 2.5, 3.3, 3.2, 3.0]
 }
 
 
 # Recibe como parametro el diccionario
 def analizar_calificaciones(calificaciones):
-    suma = 0
+    resultado = {}
+    aprobados = 0
 
     for alumno, notas in calificaciones.items():
+        suma = 0.0
         sumatoria = len(notas)
-        for numero in notas: 
+
+        for numero in notas:
             suma += numero
-            promedio = suma / sumatoria
-            if promedio >= 3.0:
-                return 0
+
+        promedio = suma / sumatoria
+        if promedio >= 3.0:
+            aprobados += 1
+
+        resultado[alumno] = promedio
+    resultado['Aprobados'] = aprobados
+
+    return resultado
+
 
 resultado = analizar_calificaciones(calificaciones)
-print(calificaciones)
-
-
-
-respuesta = {
-    'Ana': {
-        'promedio': 0,
-        'aprobado': True 
-    },
-    'Luis': {
-        'promedio': 0,
-        'aprobado': False
-    }
-}
+print(resultado)
