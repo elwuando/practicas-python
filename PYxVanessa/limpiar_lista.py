33055320186a24584a9e4426361ccba1f70a9137
Juan Arias <juandavidarias378@gmail.com>
2025-05-29 16:25:12 -0500

    Propuestas actualizadas

---
+++ PYxVanessa/limpiar_lista.py
@@ -0,0 +1,5 @@
+# Necesitamos una función en Python que reciba una lista de números
+# de contacto y devuelva una nueva lista con la mayor cantidad posible
+# de números válidos y corregidos, listos para usarse en campañas de
+# comunicación como por ejemplo WhatsApp y SMS.
+
