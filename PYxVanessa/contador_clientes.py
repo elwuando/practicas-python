a61bde9347a6f64c79fbce03085f5a4296ecde8e
Juan Arias <juandavidarias378@gmail.com>
2025-06-05 16:49:28 -0500

    nueva funcion para el registro de reportes

---
+++ PYxVanessa/contador_clientes.py
@@ -0,0 +1,65 @@
+# Diseñar una solución que permita automatizar el registro diario de clientes
+# ingresados y ventas realizadas en cada tienda Vanessa, reemplazando el
+# método actual que usa libretas y archivos de Excel desordenados.
+
+salida = {}
+
+while True:
+
+    fecha = input('Fecha (AAAA-MM-DD): ')
+    clientes = int(input('Numero de clientes: '))
+    facturas = int(input('Numero de facturas: '))
+    valor = float(input('Valor total de ventas: '))
+    almacen = input('Nombre del almacen: ')
+
+
+    reporte_cliente = {
+        'fecha': fecha,
+        'clientes': clientes,
+        'facturas': facturas,
+        'valor_ventas': valor
+    }
+
+    reportes = []
+
+    salida[almacen] = reportes
+
+    salida[almacen].append(reporte_cliente)
+    
+    continuar = input('¿Desea ingresar otro reporte? (si/no): ').lower()    
+        
+    if continuar != 'si':
+            print(f'\n{salida}')
+            break
+
+
+
+
+
+
+
+
+salida = {
+    'la33': [
+        {
+            'fecha': '2025-06-04',
+            'clientes': 99,
+            'facturas': 81,
+            'valor_ventas': 1500000
+        },
+        {
+            'fecha': '2025-06-04',
+            'clientes': 99,
+            'facturas': 81,
+            'valor_ventas': 1500000
+        },
+    ],
+    'puerta': [
+        {
+            'fecha': '2025-06-04',
+            'clientes': 99,
+            'facturas': 81,
+            'valor_ventas': 1500000
+        }
+    ]
+}
