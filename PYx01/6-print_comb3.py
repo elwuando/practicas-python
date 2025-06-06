2d3dfb7d4af912ad3beba69cda7c5f400439ca4c
Juan Arias <juandavidarias378@gmail.com>
2025-06-06 07:43:27 -0500

    Programa que imprime la mayor cantidad posible de combinaciones numéricas.

--- PYx01/6-print_comb3.py
+++ PYx01/6-print_comb3.py
@@ -1,2 +1,9 @@
 # Escribe un programa que imprima todas las posibles combinaciones
 # diferentes de dos dígitos.
+
+for numero in range(10):
+    for combinacion in range(numero + 1, 10):
+        if numero != 8 or combinacion != 9:
+            print(f"{numero}{combinacion}", end=", ")
+        else:
+            print(f"{numero}{combinacion}")
