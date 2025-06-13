69baf7e43c8ff12ec1a0bc3efd71ac6046552a9b
Juan Arias <juandavidarias378@gmail.com>
2025-06-13 16:52:15 -0500

    Avances Flask, Jinja2 y HTML

--- PYxFlask/app.py
+++ PYxFlask/app.py
@@ -1,15 +1,20 @@
+# Importa la clase datetime desde el módulo estándar datetime
 from datetime import datetime
-from flask import Flask
+# Importa la clase Flask desde el modulo Flask.
+from flask import Flask, render_template, request
 
+# Crea la app Flask y __name__ le dice a Flask desde dónde se está ejecutando la app.
 app = Flask(__name__)
 
-
+# Le dice a Flask que asocie la URL raíz (/) con una función.
 @app.route("/")
+# Esta función devuelve una cadena de texto
 def inicio():
     return "Hola, Arias. Flask está funcionando."
 
-
+# Define una ruta dinámica.
 @app.route('/user/<nombre>')
+# Define una función que recibe el valor capturado en la URL.
 def usuario(nombre):
     return f'Usuario: {nombre}'
 
@@ -36,34 +41,17 @@ def saludar(nombre):
     return f'{saludo}, {nombre}'
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/formulario', methods=['GET', 'POST'])
+def formulario():
+    nombre = None
+    celular = None
+    if request.method == 'POST':
+        print(request.form)
+        nombre = request.form.get('nombre', None)
+        celular = request.form.get('celular', None)   
+    print(nombre, celular)
+    
+    return render_template('formulario.html', persona=nombre, numero=celular)
 
 
 if __name__ == "__main__":
